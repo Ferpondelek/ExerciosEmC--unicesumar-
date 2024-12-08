c7aa18c01ca4594b088b956445703367e7402a07
Ferpondelek <104093109+Ferpondelek@users.noreply.github.com>
2024-12-08 13:07:25 -0300

    Update PROBLEMA12.c
    correção

--- PROBLEMA12.c
+++ PROBLEMA12.c
@@ -1,22 +1,24 @@
 /*O problema consiste em auxiliar um professor no fechamento das notas de uma
-turma. Para tanto, deve ser construído um programa que leia o código do aluno,
-o número de notas da disciplina e as notas. Calcule a média final de cada aluno e
-informe o número de alunos aprovados e reprovados. Para ser aprovado, o estudante precisa obter média maior ou igual a seis. O programa é encerrado quando
-é informado o código de aluno zero.*/
+turma. Para tanto, deve ser construÃ­do um programa que leia o cÃ³digo do aluno,
+o nÃºmero de notas da disciplina e as notas. Calcule a mÃ©dia final de cada aluno e
+informe o nÃºmero de alunos aprovados e reprovados. Para ser aprovado, o estudante precisa obter mÃ©dia maior ou igual a seis. O programa Ã© encerrado quando
+Ã© informado o cÃ³digo de aluno zero.*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int cod,qtdProvas,i;
+    int cod,qtdProvas,i,aprovados=0,reprovados=0;
     float notas,TotalN,media;
     TotalN=0,media=0;
     printf("Digite o codigo do aluno: \n");
     scanf("%d",&cod);
-    while(cod!=0) {
+    while(cod!=0)
+    {
         printf("Digite a quantidade de provas: \n");
         scanf("%d",&qtdProvas);
-        for(i=0;i<qtdProvas;i++){
+        for(i=0; i<qtdProvas; i++)
+        {
             printf("Digite a nota %d \n",i+1);
             scanf("%f",&notas);
             TotalN+=notas;
@@ -24,12 +26,23 @@ int main()
         media = TotalN/qtdProvas;
         TotalN=0;
         printf("media do aluno %d : %.2f\n",cod,media);
+         if(media>=6.00)
+        {
+            aprovados++;
+        }
+        else
+        {
+            reprovados++;
+        }
         media=0;
 
         printf("Digite o codigo do aluno: \n");
         scanf("%d",&cod);
 
 
+
     }
+
+    printf("Aprovados : %d\nReprovados : %d",aprovados,reprovados);
     return 0;
 }
